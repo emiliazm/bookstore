bf9fdaa8c15275bd3687de8039ef21414c9f9062
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-06-07 22:16:17 +0200

    Add funtionality to remove a book.

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
 const Book = (props) => {
   const { id, bookTitle, author } = props;
+  const dispatch = useDispatch();
+
+  const removeHandler = () => {
+    dispatch(removeBook(id));
+  };
+
   return (
     <li className="item" id={id}>
       <h2>{bookTitle}</h2>
       by
       <p>{author}</p>
-      <button id={id} className="remove" type="button">
+      <button id={id} className="remove" type="button" onClick={removeHandler}>
         Remove
       </button>
     </li>
@@ -16,7 +24,7 @@ const Book = (props) => {
 };
 
 Book.propTypes = {
-  id: PropTypes.number.isRequired,
+  id: PropTypes.string.isRequired,
   bookTitle: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
 };
