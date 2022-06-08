f8f58a4d5f946d886403cf2edbd1d3d26785e761
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-06-08 20:41:06 +0200

    Import thunk.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/configureStore';
+import { getBook } from './redux/books/books';
+
+store.dispatch(getBook());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 import bookReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
@@ -7,6 +8,7 @@ const store = configureStore({
     books: bookReducer,
     categories: categoriesReducer,
   },
+  middleware: [thunk],
 });
 
 export default store;
